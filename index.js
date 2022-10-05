72a453686377f40831a9e466fa9ae7d2b097f9ea
Ardy James Raagas <ardy.raagas@ardy.raagas-CP2JWXM177>
2022-10-05 10:57:29 +0800

    add whitespace

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const {square, fahrenheit,rectangleArea,isPalindrome} = require ('./lib')
 
 console.log(square(2));
 console.log(fahrenheit(60));
-console.log(rectangleArea(2,4));
+console.log(rectangleArea(2, 4));
 console.log(isPalindrome('kayak'));
 console.log(isPalindrome('money'));
