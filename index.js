bd2c57db2620a8247bcfdb28c3d1c988e1213a85
Ardy James Raagas <ardy.raagas@ardy.raagas-CP2JWXM177>
2022-10-05 10:15:15 +0800

    testing function

---
+++ index.js
@@ -0,0 +1,6 @@
+const {square, fahrenheit,rectangleArea,isPalindrome} = require ('./lib')
+
+console.log(square(2));
+console.log(fahrenheit(60));
+console.log(rectangleArea(2,4));
+console.log(isPalindrome('kayak'));
