07c91133075a02a3e80b716330b8b474f5171983
Ardy James Raagas <ardy.raagas@ardy.raagas-CP2JWXM177>
2022-10-05 08:45:22 +0800

    Created function in exercise2

---
+++ lib.js
@@ -0,0 +1,7 @@
+const square = (n) => n ** 2;
+
+const fahrenheit = (celsius) => (celsius * 1.8) + 32;
+
+const rectangleArea = (a, b) => a * b;
+
+const isPalindrome = (str) => str.toUpperCase() === str.toUpperCase().split('').reverse().join('');
